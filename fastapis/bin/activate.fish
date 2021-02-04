6f93eb66e5994ddd47fc06e1d9813016a18b5d53
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2021-02-04 16:53:01 +0330

    FastAPI Tutorial 11

--- fastapis/bin/activate.fish
+++ fastapis/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/mrk2/fastapi/FatAPI-Tutorial/fastapis'
+set -gx VIRTUAL_ENV '/home/mrk2/fastapi/FastAPI-Tutorial/fastapis'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
--- sql_app/database.py
+++ sql_app/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:fastapi_pass@localhost/fastapi_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
